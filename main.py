cdbf546124021b8b47efbe7011aa9edda07cdcb3
Egor Almikeev <york.git@gmail.com>
2017-11-21 21:16:03 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+keys = [1, 2, 3, 4, 5]
+values = ['one', 'two', 'three']
+
+def make_dict(keys, values):
+
+	"""
+	Arguments must be iterable and indexable.
+	Function returns a dictionary with keys
+	from first argument elements, and values
+	from second elements argument.
+	If length of the second argument less
+	than lenght of the first argument,
+	difference will fills with None values.
+	"""
+
+	dictionary = dict()
+	for i in range(len(keys)):
+		if i > len(values) - 1:
+			dictionary.update({keys[i] : None})
+		else:
+			dictionary.update({ keys[i] : values[i]})
+
+	return dictionary
+
+
+print(make_dict(keys, values))
